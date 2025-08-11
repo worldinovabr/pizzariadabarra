649b284be2284927805f3eaa728996d384bfe0bb
Weslei BBS <wesleibbs@gmail.com>
2025-08-11 18:59:01 -0300

    Atualiza projeto

--- docs/dashboard/firebaseConfig.js
+++ docs/dashboard/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AZaSyAM6eaqF763bjoeXQV5kECxzed9ZqkLiLs",
+  apiKey: "AIzaSyAM6eaqF763bjoeXQV5kECkzed9ZqkLiLs",
   authDomain: "pizzaria-bd59b.firebaseapp.com",
   projectId: "pizzaria-bd59b",
-  storageBucket: "pizzaria-bd59b.appspot.com",
+  storageBucket: "pizzaria-bd59b.firebasestorage.app",
   messagingSenderId: "947433117718",
-  appId: "1:947433117718:web:15cdd8a158e063e8723d37",
+  appId: "1:947433117718:web:15cdd0a158e063e8723d37",
   measurementId: "G-JFYSR2PPHJ"
 };
