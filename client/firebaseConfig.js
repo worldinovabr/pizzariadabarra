5773a5dd32b9ec5493180c221c1b7cb2909e47a5
Weslei BBS <wesleibbs@gmail.com>
2025-08-12 11:41:24 -0300

    Atualiza projeto

--- client/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// firebaseConfig.js - substitua com as suas credenciais do Firebase
-export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
